ded0438bfacbc14988598cca6fc3ae9dbfe71bec
Your Name <youremail@example.com>
2025-07-11 18:27:33 +0530

    Initial commit

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:Vini0911%40@localhost/personalupi"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://sql5789495:mK3xmCSmIp@sql5.freesqldatabase.com/sql5789495"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
